afb2982281dd617af05209f500be22cbd98a2049
Ivan <shilkin-ivan@mail.ru>
2022-12-14 20:28:40 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿void Cube (int[] array, int s)
+{
+    for (int i = 0; i < s; i++)
+    {
+        double cube = Math.Pow(array[i],3);
+        Console.WriteLine (cube);
+    }
+}
+Console.WriteLine ("Введите число ");
+int n = int.Parse(Console.ReadLine());
+int[] a = new int[n];
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = i + 1;
+}
+Cube (a,n);

9bbe482b5a03d8f05eb85cdfeda372fcc5803b8d
Ivan <shilkin-ivan@mail.ru>
2022-12-17 12:44:24 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,10 +3,15 @@ string n = Console.ReadLine();
 int l = n.Length;
 if (l==5)
 {
-     if (n[0]==n[4] || n[1]==n[3])
-    
-     Console.WriteLine("Палиндром");
-
+     if (n[0]==n[4])
+     {
+          if(n[1]==n[3])
+          {
+            Console.WriteLine("Палиндром");   
+          }
+          else
+            Console.WriteLine("Не палиндром");
+     }
      else
      Console.WriteLine("Не палиндром");
 }
